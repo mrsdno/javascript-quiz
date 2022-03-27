0370adfff9dd6fdca14011e3ac3d442fe043a605
mrsdno <christine@dno.dev>
2022-03-27 15:21:32 -0400

    styling; added button functionality and timer

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,124 @@
-var startQuizEl = document.querySelector('#start-quiz');
 
-var quizEl = document.querySelector('#quiz');
+var startButton = document.getElementById('start-btn');
+var questionContainer = document.getElementById('question-container');
+var timerEl = document.getElementById('timer');
 
 
-var startQuiz = function () {
-    var counter = 5;
+// add listener to start button
+startButton.addEventListener('click', startQuiz);
 
+function startQuiz() {
+    startButton.classList.add('hide');
+    questionContainer.classList.remove('hide');
+
+    startTimer();
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+function startTimer() {
     //start timer when you click the button
-    span = document.getElementById("counter");
-    span.innerHTML= counter;
+    console.log("timer started!")
+    var timeLeft = 10;
+    console.log(timeLeft);
 
-    //decrease timer 
-    setInterval(function() {
-        if (counter >= 0) {
-            span.innerHTML= counter;
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' second remaining';
+            timeLeft--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            displayMessage();
         }
-        if (counter === 0) {
-            alert('Sorry, out of time!');
-            clearInterval(counter);
-        }
-        counter--;
     }, 1000);
+}
 
-    //run diplay question function
-    displayQuestion();
-};
+function displayMessage() {
+    timerEl.textContent = "Time's up!";
+}
 
-var displayQuestion = function () {
-    var quizQuestionEl = document.createElement("p"); 
-    quizQuestionEl.innerHTML = "This is the first question!";
-    quizEl.appendChild(quizQuestionEl);
-};
 
-startQuizEl.addEventListener("click", startQuiz);
+
+
+// var startQuizEl = document.querySelector('#start-quiz');
+// var quizEl = document.querySelector('#quiz');
+
+// var quizQuestions = [
+//     {
+//      prompt: "This is question 1?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
+//      answer: "a"
+//     },
+//     {
+//      prompt: "This is question 2?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
+//      answer: "a"
+//     },
+//     {
+//      prompt: "This is question 3?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
+//      answer: "a"
+//     },
+//     {
+//      prompt: "This is question 4?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
+//      answer: "a"
+//     },
+// ];
+
+// var score = 0;
+
+// var startQuiz = function () {
+//     var counter = 5;
+
+//     //run diplay question function
+//     displayQuestion();
+
+//     //start timer when you click the button
+//     div = document.getElementById("counter");
+
+//     div.innerHTML=counter;
+
+//     //decrease timer 
+//     setInterval(function() {
+//         if (counter >= 0) {
+//             div.innerHTML= counter;
+//         }
+//         if (counter === 0) {
+//             alert('Sorry, out of time!');
+//             clearInterval(counter);
+//         }
+//         counter--;
+//     }, 1000);
+// };
+
+// var displayQuestion = function () {
+    
+
+//     for(var i=0; i < quizQuestions.length; i++) {
+//         var displayQuestionEl = document.createElement("div");
+//         displayQuestionEl=quizQuestions[i]
+//         quizEl.appendChild(displayQuestionEl);
+//     //     var response = window.prompt(quizQuestions[i].prompt);
+
+//     //     if(response == quizQuestions[i].answer) {
+//     //         score++;
+//     //         alert("Correct");
+//     //     }
+
+//     //     else {
+//     //         alert("Wrong");
+//     //     }
+//     // }
+
+//     // alert("you got " + score + "/" + quizQuestions.length);
+//     }
+// };
+
+// startQuizEl.addEventListener("click", startQuiz);
