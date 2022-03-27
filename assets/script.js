7dfde55999f45b0f28922db6a6e943c8ae59bdea
mrsdno <christine@dno.dev>
2022-03-27 16:25:47 -0400

    display first question after hitting start button

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,73 @@
 
 var startButton = document.getElementById('start-btn');
+var nextButton = document.getElementById('next-btn');
 var questionContainer = document.getElementById('question-container');
 var timerEl = document.getElementById('timer');
+var questionElement = document.getElementById('question');
+var answerButtonsEl = document.getElementById('answer-buttons');
+
+const questions = [
+ {
+  question: "This is question 1?",
+  answers: [
+    { text: 'a', correct: true },
+    { text: 'b', correct: false },
+    { text: 'c', correct: false },
+    { text: 'd', correct: false },
+   ]
+ }
+]
 
 
 // add listener to start button
 startButton.addEventListener('click', startQuiz);
 
+
 function startQuiz() {
+    //hide the startbutton, and display the question container
     startButton.classList.add('hide');
     questionContainer.classList.remove('hide');
 
+    // display the next question
+    nextQuestion();
+
+    // start the countdown timer
     startTimer();
 }
 
 function nextQuestion() {
+    //clear the question container to prepare to append answer options
+    clearQuestionContainer();
 
+    //display the first question
+    let question = questions[0].question;
+    questionElement.textContent = question;
+
+    //loop through answer options and append them to the container
+    questions[0].answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+
+        //assign a string to the correct answer if it is selected
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+
+        //call select answer function if button is clicked 
+        button.addEventListener('click', selectAnswer);
+
+        answerButtonsEl.appendChild(button);
+    })
+}
+
+function clearQuestionContainer() {
+
+    //hide next button and remove default answer options
+    nextButton.classList.add('hide');
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+    }
 }
 
 function selectAnswer() {
@@ -23,10 +75,9 @@ function selectAnswer() {
 }
 
 function startTimer() {
-    //start timer when you click the button
-    console.log("timer started!")
-    var timeLeft = 10;
-    console.log(timeLeft);
+    
+    //set time to 60 seconds
+    var timeLeft = 60;
 
     var timeInterval = setInterval(function() {
         if (timeLeft > 1) {
@@ -38,65 +89,12 @@ function startTimer() {
         } else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
-            displayMessage();
+            timerEl.textContent = "Time's up!";
         }
     }, 1000);
 }
 
-function displayMessage() {
-    timerEl.textContent = "Time's up!";
-}
 
-
-
-
-// var startQuizEl = document.querySelector('#start-quiz');
-// var quizEl = document.querySelector('#quiz');
-
-// var quizQuestions = [
-//     {
-//      prompt: "This is question 1?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
-//      answer: "a"
-//     },
-//     {
-//      prompt: "This is question 2?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
-//      answer: "a"
-//     },
-//     {
-//      prompt: "This is question 3?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
-//      answer: "a"
-//     },
-//     {
-//      prompt: "This is question 4?\n(a) Answer A\n(b) Answer B\n(c) Answer C",
-//      answer: "a"
-//     },
-// ];
-
-// var score = 0;
-
-// var startQuiz = function () {
-//     var counter = 5;
-
-//     //run diplay question function
-//     displayQuestion();
-
-//     //start timer when you click the button
-//     div = document.getElementById("counter");
-
-//     div.innerHTML=counter;
-
-//     //decrease timer 
-//     setInterval(function() {
-//         if (counter >= 0) {
-//             div.innerHTML= counter;
-//         }
-//         if (counter === 0) {
-//             alert('Sorry, out of time!');
-//             clearInterval(counter);
-//         }
-//         counter--;
-//     }, 1000);
-// };
 
 // var displayQuestion = function () {
     
@@ -116,9 +114,3 @@ function displayMessage() {
 //     //         alert("Wrong");
 //     //     }
 //     // }
-
-//     // alert("you got " + score + "/" + quizQuestions.length);
-//     }
-// };
-
-// startQuizEl.addEventListener("click", startQuiz);
