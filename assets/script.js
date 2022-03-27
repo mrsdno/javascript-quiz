b43c69b80341ef31a2f0b296f830948551be7aa7
mrsdno <christine@dno.dev>
2022-03-27 13:14:34 -0400

    updated timer and added first question display

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,35 @@
 var startQuizEl = document.querySelector('#start-quiz');
-var counter = 60;
+
+var quizEl = document.querySelector('#quiz');
+
 
 var startQuiz = function () {
+    var counter = 5;
+
     //start timer when you click the button
+    span = document.getElementById("counter");
+    span.innerHTML= counter;
+
+    //decrease timer 
     setInterval(function() {
-        counter--;
         if (counter >= 0) {
-            span = document.getElementById("counter");
-            span.innerHTML= counter;   
+            span.innerHTML= counter;
         }
         if (counter === 0) {
             alert('Sorry, out of time!');
             clearInterval(counter);
         }
+        counter--;
     }, 1000);
-}
+
+    //run diplay question function
+    displayQuestion();
+};
 
 var displayQuestion = function () {
-    
-}
+    var quizQuestionEl = document.createElement("p"); 
+    quizQuestionEl.innerHTML = "This is the first question!";
+    quizEl.appendChild(quizQuestionEl);
+};
 
 startQuizEl.addEventListener("click", startQuiz);
